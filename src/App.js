14e1eb4069df78e525ac7e06b67da0b1547c5a52
zilani131 <zilani.cuet@gmail.com>
2022-03-16 19:26:24 +0600

    Load dynamic data, API call useEffect integrate state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App(pros) {
 
   return (
     <div className="App">
-     <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
-function Counter(pros){
-  const [count,setCount]=useState(3);
-  const increaseCount=()=>setCount(count+1)
-  const decreaseCount=()=>setCount(count-1)
-  // const newCount=count+1;
-  // setCount(newCount)}
- 
-
+function ExternalUsers(){
+  // useState default value has been given an empty array
+  // just remember upcoming class we will know what happens
+  const [user,setUser]=useState([]);
+  // without second bracket in useEffect ,
+  // render will not occur and it will run infinite loop
+  useEffect(()=>{
+   fetch("https://jsonplaceholder.typicode.com/users")
+   .then(res=>res.json())
+   .then(user=>setUser(user))
+  },[])
   return (
-    <div className='counter-style'>
-      <h1>Counter: {count}</h1>
-      <button onClick={increaseCount}>Increasing</button>
-      <button onClick={decreaseCount}>Decreasing</button>
-      
+    <div>
+     
+      {user.map(users=> <Person name={users.name} email={users.email}></Person>) }
+
     </div>
   )
 }
+function Person(props){
+  console.log(props)
+  return (
+    <div style={{border:"2px solid blue",borderRadius:"20px",backgroundColor:"lightblue",margin:"20px auto" ,width:"50%"}}>
+      <h1>Name:{props.name}</h1>
+      <p>Email:{props.email}</p>
+    </div>
+  )
+}
+// function Counter(pros){
+//   const [count,setCount]=useState(3);
+//   const increaseCount=()=>setCount(count+1)
+//   const decreaseCount=()=>setCount(count-1)
+//   // const newCount=count+1;
+//   // setCount(newCount)}
+ 
+
+//   return (
+//     <div className='counter-style'>
+//       <h1>Counter: {count}</h1>
+//       <button onClick={increaseCount}>Increasing</button>
+//       <button onClick={decreaseCount}>Decreasing</button>
+      
+//     </div>
+//   )
+// }
 
 export default App;
