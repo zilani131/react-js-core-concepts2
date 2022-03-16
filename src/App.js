d47c8ff57854e428e8cd0470b967e8cfc7a732f4
zilani131 <zilani.cuet@gmail.com>
2022-03-16 16:35:05 +0600

    State, component state hook and set state method

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App(pros) {
-  const products=[{name:"laptop",price:75000},
-                  {name:"mobile",price:50000},
-                {name:"TV",price:40000}]
+
   return (
     <div className="App">
-     {products.map(product=><Product name={product.name} price={product.price}></Product>)}
+     <Counter></Counter>
     </div>
   );
 }
-function Product(pros){
+function Counter(pros){
+  const [count,setCount]=useState(3);
+  const increaseCount=()=>setCount(count+1)
+  const decreaseCount=()=>setCount(count-1)
+  // const newCount=count+1;
+  // setCount(newCount)}
+ 
+
   return (
-    <div className='product-style'>
-      <h1>Product:{pros.name}</h1>
-      <h2>Price:{pros.price}</h2>   
+    <div className='counter-style'>
+      <h1>Counter: {count}</h1>
+      <button onClick={increaseCount}>Increasing</button>
+      <button onClick={decreaseCount}>Decreasing</button>
+      
     </div>
   )
 }
